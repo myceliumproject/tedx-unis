3b06d01e98235dadb0374cae5cdad403eb2b08fc
DiegoV <ddvallejoj@gmail.com>
2023-10-08 15:53:56 -0600

    env

--- back/src/index.js
+++ back/src/index.js
@@ -7,6 +7,10 @@ import morgan from "morgan";
 import { Server } from "socket.io";
 import eventBlockRouter from "./controllers/eventblock.js";
 import userRouter from "./controllers/user.js";
+import { db } from "./db.js";
+import { config } from "dotenv";
+
+config();
 
 const app = express();
 const server = createServer(app);
@@ -28,6 +32,144 @@ app.use(
 
 app.use("/user", userRouter);
 app.use("/eventblock", eventBlockRouter);
+app.get("/hello", (req, res) => {
+  res.json("hello!");
+});
+
+/** TEST API **/
+app.get("/test", async (req, res) => {
+  try {
+    const data = req.body;
+    const docRef = await db.collection('test').add(data);
+
+    res.status(201).json({ id: docRef.id });
+  } catch (error) {
+    res.status(500).json({ error: 'Test Error' });
+  }
+})
+
+/** EMPTY EVENT BLOCKS **/
+app.post('/eventblock', async (req, res) => {
+  try {
+    const eventBlock = {
+      datetime: '', 
+      img: '', // URL
+      events: [
+        {
+          name: '',
+          speaker: '',
+          description: '',
+          speakerImg: '', // URL
+        },
+        {
+          name: '',
+          speaker: '',
+          description: '',
+          speakerImg: '', // URL
+        },
+        {
+          name: '',
+          speaker: '',
+          description: '',
+          speakerImg: '', // URL
+        },
+      ],
+      takenSeats: [],
+      takenSeatAssignments: [],
+      waitlist: [],
+    };
+
+    const docRef = await db.collection('event_blocks').add(eventBlock);
+
+    res.status(201).json({ id: docRef.id });
+  } catch (error) {
+    res.status(500).json({ error: 'Error 500' });
+  }
+})
+
+/** GET ALL EVENT BLOCKS **/
+app.get('/eventblock/list', async (req, res) => {
+  try {
+    const eventBlocksSnapshot = await db.collection('event_blocks').get();
+    const eventBlocks = [];
+
+    eventBlocksSnapshot.forEach((doc) => {
+      const eventBlockData = doc.data();
+      eventBlocks.push({ id: doc.id, ...eventBlockData });
+    });
+
+    res.status(200).json({code: 0, data: eventBlocks});
+  } catch (error) {
+    res.status(500).json({ error: 'Error 500' });
+  }
+});
+
+/** SPECIFIC EVENT BLOCK **/
+app.get('/eventblock/get/:eventBlockId', async (req, res) => {
+  try {
+    const { eventBlockId } = req.params;
+
+    const eventBlockRef = db.collection('event_blocks').doc(eventBlockId);
+    const eventBlockSnapshot = await eventBlockRef.get();
+    const eventBlockData = eventBlockSnapshot.data();
+
+    res.status(200).json({code: 0, data: eventBlockData});
+  } catch (error) {
+    res.status(500).json({ error: 'Error 500' });
+  }
+});
+
+
+/** RESERVE 
+body:
+{
+  "userId": "Test", 
+  "name": "Test", 
+  "seat": "A23"
+}
+ * **/
+
+app.patch('/eventblock/reserve/:id_eventblock', async (req, res) => {
+  try {
+    const { id_eventblock } = req.params;
+    const { userId, name, seat } = req.body;
+
+    const eventBlockRef = db.collection('event_blocks').doc(id_eventblock);
+    const eventBlockSnapshot = await eventBlockRef.get();
+
+    const eventBlockData = eventBlockSnapshot.data();
+    const takenSeatAssignments = eventBlockData.takenSeatAssignments
+
+    /**Check if User Exists */
+    const existingAssignment = takenSeatAssignments.find(
+      (assignment) => assignment.userId === userId
+    );
+    if (existingAssignment) {
+      return res.status(400).json({ error: 'User already has a seat assigned' });
+    }
+
+    const newAssignment = { userId, name, seat };
+    takenSeatAssignments.push(newAssignment);
+
+    const takenSeats = eventBlockData.takenSeats
+    if (takenSeats.includes(seat)) {
+      return res.status(400).json({ error: 'Seat already taken' });
+    }
+    else {
+      takenSeats.push(seat);
+    }
+
+    await eventBlockRef.update({
+      takenSeatAssignments,
+      takenSeats,
+    });
+    res.status(200).json({ message: 'Success' });
+  } catch (error) {
+    res.status(500).json({ error: 'Error 500' });
+  }
+});
+
+
 
 io.on("connection", (socket) => {
   console.log("socket connected");
