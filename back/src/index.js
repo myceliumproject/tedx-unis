3708f0d8418670e78825484d94a65c65fc5720be
DiegoV <ddvallejoj@gmail.com>
2023-10-08 12:50:19 -0600

    'apis_iniciales_bloques'

--- back/src/index.js
+++ back/src/index.js
@@ -5,6 +5,7 @@ import express from "express";
 import { createServer } from "http";
 import morgan from "morgan";
 import { Server } from "socket.io";
+import { db } from "./db.js";
 
 const app = express();
 const server = createServer(app);
@@ -27,6 +28,125 @@ app.use(
 app.get("/hello", (req, res) => {
   res.json("hello!");
 });
+
+/** TEST API **/
+app.get("/test", async (req, res) => {
+  try {
+    const data = req.body;
+    const docRef = await db.collection('test').add(data);
+
+    res.status(201).json({ id: docRef.id });
+  } catch (error) {
+    res.status(500).json({ error: 'Test Error' });
+  }
+})
+
+/** EMPTY EVENT BLOCKS **/
+app.post('/event_block', async (req, res) => {
+  try {
+    const eventBlock = {
+      datetime: '', 
+      img: '', // URL
+      events: [
+        {
+          name: '',
+          speaker: '',
+          description: '',
+          speakerImg: '', // URL
+        },
+        {
+          name: '',
+          speaker: '',
+          description: '',
+          speakerImg: '', // URL
+        },
+        {
+          name: '',
+          speaker: '',
+          description: '',
+          speakerImg: '', // URL
+        },
+      ],
+      takenSeats: [],
+      takenSeatAssignments: [],
+      waitlist: [],
+    };
+
+    const docRef = await db.collection('event_blocks').add(eventBlock);
+
+    res.status(201).json({ id: docRef.id });
+  } catch (error) {
+    res.status(500).json({ error: 'Error 500' });
+  }
+})
+
+/** GET ALL EVENT BLOCKS **/
+app.get('/event_blocks', async (req, res) => {
+  try {
+    const eventBlocksSnapshot = await db.collection('event_blocks').get();
+    const eventBlocks = [];
+
+    eventBlocksSnapshot.forEach((doc) => {
+      const eventBlockData = doc.data();
+      eventBlocks.push({ id: doc.id, ...eventBlockData });
+    });
+
+    res.status(200).json(eventBlocks);
+  } catch (error) {
+    res.status(500).json({ error: 'Error 500' });
+  }
+});
+
+/** RESERVE 
+{
+  "userId": "Test", 
+  "name": "Test", 
+  "seat": "A23"
+}
+ * **/
+
+app.patch('/reserve/:id_event_block', async (req, res) => {
+  try {
+    const { id_event_block } = req.params;
+    const { userId, name, seat } = req.body;
+
+    const eventBlockRef = db.collection('event_blocks').doc(id_event_block);
+    const eventBlockSnapshot = await eventBlockRef.get();
+
+    const eventBlockData = eventBlockSnapshot.data();
+    const takenSeatAssignments = eventBlockData.takenSeatAssignments
+
+    /**Check if User Exists */
+    const existingAssignment = takenSeatAssignments.find(
+      (assignment) => assignment.userId === userId
+    );
+    if (existingAssignment) {
+      return res.status(400).json({ error: 'User already has a seat assignment' });
+    }
+
+    const newAssignment = { userId, name, seat };
+    takenSeatAssignments.push(newAssignment);
+
+    const takenSeats = eventBlockData.takenSeats
+    if (takenSeats.includes(seat)) {
+      return res.status(400).json({ error: 'Seat already taken' });
+    }
+    else {
+      takenSeats.push(seat);
+    }
+
+    await eventBlockRef.update({
+      takenSeatAssignments,
+      takenSeats,
+    });
+
+    res.status(200).json({ message: 'Success' });
+  } catch (error) {
+    res.status(500).json({ error: 'Error 500' });
+  }
+});
+
+
 
 io.on("connection", (socket) => {
   console.log("socket connected");
