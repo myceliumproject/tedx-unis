f08bb17171fc4d2cca525da0fb407b986041bd83
jrberganza <68700999+jrberganza@users.noreply.github.com>
2023-10-08 08:33:34 -0600

    Login pages

---
+++ front/src/pages/staff/StaffRegister.jsx
@@ -0,0 +1,32 @@
+export default function StaffRegister() {
+  return (
+    <div className="p-3 text-center d-flex align-items-center justify-content-center align-self-center">
+      <div className="border p-4 align-items-center">
+        <h1 className="text-center">Registro Staff</h1>
+        <div className="form-floating mb-3">
+          <input
+            className="form-control rounded"
+            id="name"
+            placeholder="Nombre"
+          />
+          <label for="name">Nombre</label>
+        </div>
+        <div className="form-floating mb-3">
+          <input
+            type="email"
+            id="email"
+            className="form-control rounded"
+            placeholder="Correo electrónico"
+          />
+          <label for="email">Correo electrónico</label>
+        </div>
+        <button
+          type="button"
+          className="btn btn-primary align-self-center rounded text-white"
+        >
+          Registrarse
+        </button>
+      </div>
+    </div>
+  );
+}
--- front/src/routes/index.tsx
+++ front/src/routes/index.tsx
@@ -1,8 +1,13 @@
 import MainLayout from "$/layouts/MainLayout";
+import Auth from "$/pages/Auth";
 import BlockInfo from "$/pages/BlockInfo";
 import Home from "$/pages/Home";
 import type { RouteObject } from "react-router-dom";
 import NotFound from "./NotFound";
+// Staff
+import ListaBloques from "$/pages/staff/ListaBloques";
+import StaffLogin from "$/pages/staff/StaffLogin";
+import StaffRegister from "$/pages/staff/StaffRegister";
 
 const routes: RouteObject[] = [
   {
@@ -16,6 +21,29 @@ const routes: RouteObject[] = [
         path: "/block/:id",
         element: <BlockInfo />,
       },
+      {
+        path: "/auth",
+        element: <Auth />,
+      },
+      {
+        path: "*",
+        element: <NotFound />,
+      },
+    ],
+  },
+  {
+    path: "/staff",
+    element: <MainLayout />,
+    children: [
+      {
+        path: "/staff/",
+        element: <ListaBloques />,
+      },
+      {
+        path: "/staff/register",
+        element: <StaffRegister />,
+      },
+      { path: "/staff/login", element: <StaffLogin /> },
       {
         path: "*",
         element: <NotFound />,
