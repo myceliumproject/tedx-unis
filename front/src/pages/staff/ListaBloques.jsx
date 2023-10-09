5b90401e53b7c3429f61525db58f6a5665489e71
jrberganza <68700999+jrberganza@users.noreply.github.com>
2023-10-08 20:59:47 -0600

    Remove last reference to staticData

--- front/src/pages/staff/ListaBloques.jsx
+++ front/src/pages/staff/ListaBloques.jsx
@@ -1,5 +1,6 @@
+import { urlApi } from "$/environment";
 import EventBlock from "$/lib/components/EventBlock";
-import * as staticData from "$/lib/staticData";
+import axios from "axios";
 import { useLayoutEffect, useState } from "react";
 import { Stack } from "react-bootstrap";
 
@@ -7,13 +8,14 @@ export default function ListaBloques() {
   const [eventBlocks, setEventBlocks] = useState([]);
 
   useLayoutEffect(() => {
-    // axios.get(urlApi + `/eventblock/list`).then((res) => {
-    //   if (res.data.code === 0) {
-    //     setEventBlocks(res.data.data);
-    //   }
-    // });
-    setEventBlocks(staticData.eventBlocks);
+    axios.get(urlApi + `/eventblock/list`).then((res) => {
+      if (res.data.code === 0) {
+        //console.log(res.data)
+        setEventBlocks(res.data.data);
+      }
+    });
   }, []);
+
   return (
     <div>
       <h1 className="text-center">Bloques</h1>
