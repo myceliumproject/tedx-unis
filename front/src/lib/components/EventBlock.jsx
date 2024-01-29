4fe7e3e78d99dde20ba4922459612dbb2f2bd84b
Luis Menendez <dvallejo@e-gsi.net>
2024-01-28 23:10:38 -0600

    front update bloque

--- front/src/lib/components/EventBlock.jsx
+++ front/src/lib/components/EventBlock.jsx
@@ -1,70 +1,92 @@
 // @ts-check
-import { Accordion, Badge, Card, Col, Row } from "react-bootstrap";
-import { MdCheck } from "react-icons/md";
+import { useState } from "react";
+import { Accordion, Badge, Button, Card, Col, Modal, ModalHeader, Row } from "react-bootstrap";
+import { MdCheck, MdEdit } from "react-icons/md";
 import { Link } from "react-router-dom";
 
-export default function EventBlock({ page = null, seat = null, data }) {
+export default function EventBlock({ page = null, seat = null, data, setModalEdit, setCurrentEdit }) {
+  const [hover, setHover] = useState(false);
+
   return (
-    <Card border={seat !== null ? "primary" : undefined}>
-      <Card.Img
-        src={data?.img}
-        className="object-fit-cover"
-        style={{ height: "16rem" }}
-      />
-      {seat !== null ? (
-        <Badge
-          color="primary text-nowrap"
-          style={{ position: "absolute", top: "1rem", left: "1rem" }}
-          pill
-        >
-          <MdCheck /> Reservado: {seat}
-        </Badge>
-      ) : null}
-      <Card.Body>
-        <Card.Subtitle>
-          {new Date(data?.datetime).toLocaleDateString()} -{" "}
-          {new Date(data?.datetime).toLocaleTimeString()}
-        </Card.Subtitle>
-      </Card.Body>
-      <Accordion
-        alwaysOpen
-        flush
-        className="border-top border-bottom border-tertiary"
+    <>
+      <Card
+        border={seat !== null ? "primary" : undefined}
+        style={{ position: "relative" }}
+        onMouseEnter={() => setHover(true)}
+        onMouseLeave={() => setHover(false)}
       >
-        {data?.events.map((ev, i) => (
-          <Accordion.Item eventKey={i.toString()} key={i}>
-            <Accordion.Header>
-              {ev.name} - {ev.speaker}
-            </Accordion.Header>
-            <Accordion.Body className="p-0">
-              <Row>
-                <Col xs={4}>
-                  <img src={ev.speakerImg} width="100%" />
-                </Col>
-                <Col xs={8} className="p-2">
-                  {ev.description}
-                </Col>
-              </Row>
-            </Accordion.Body>
-          </Accordion.Item>
-        ))}
-      </Accordion>
-      {page !== null ? (
+        {page === "staff" && hover && (
+          <Button
+            variant="primary"
+            style={{ position: "absolute", top: "10px", right: "10px" }}
+            onClick={() => {
+              setCurrentEdit(data)
+              setModalEdit(true)
+            }}
+          >
+            <MdEdit /> Editar
+          </Button>
+        )}
+        <Card.Img
+          src={data?.img}
+          className="object-fit-cover"
+          style={{ height: "16rem" }}
+        />
+        {seat !== null ? (
+          <Badge
+            color="primary text-nowrap"
+            style={{ position: "absolute", top: "1rem", left: "1rem" }}
+            pill
+          >
+            <MdCheck /> Reservado: {seat}
+          </Badge>
+        ) : null}
         <Card.Body>
-          {page === "home" ? (
-            <Link to={`/block/${data?.id}`} className="btn btn-primary">
-              {seat !== null ? "Ver ticket" : "Conseguir asiento"}
-            </Link>
-          ) : page === "staff" ? (
-            <Link
-              to={`/staff/attendance/${data?.id}`}
-              className="btn btn-primary"
-            >
-              Lista de asignados
-            </Link>
-          ) : null}
+          <Card.Subtitle>
+            {new Date(data?.datetime).toLocaleDateString()} -{" "}
+            {new Date(data?.datetime).toLocaleTimeString()}
+          </Card.Subtitle>
         </Card.Body>
-      ) : null}
-    </Card>
+        <Accordion
+          alwaysOpen
+          flush
+          className="border-top border-bottom border-tertiary"
+        >
+          {data?.events.map((ev, i) => (
+            <Accordion.Item eventKey={i.toString()} key={i}>
+              <Accordion.Header>
+                {ev.name} - {ev.speaker}
+              </Accordion.Header>
+              <Accordion.Body className="p-0">
+                <Row>
+                  <Col xs={4}>
+                    <img src={ev.speakerImg} width="100%" />
+                  </Col>
+                  <Col xs={8} className="p-2">
+                    {ev.description}
+                  </Col>
+                </Row>
+              </Accordion.Body>
+            </Accordion.Item>
+          ))}
+        </Accordion>
+        {page !== null ? (
+          <Card.Body>
+            {page === "home" ? (
+              <Link to={`/block/${data?.id}`} className="btn btn-primary">
+                {seat !== null ? "Ver ticket" : "Conseguir asiento"}
+              </Link>
+            ) : page === "staff" ? (
+              <Link
+                to={`/staff/attendance/${data?.id}`}
+                className="btn btn-primary"
+              >
+                Lista de asignados
+              </Link>
+            ) : null}
+          </Card.Body>
+        ) : null}
+      </Card>
+    </>
   );
 }
--- front/src/pages/staff/ListaBloques.jsx
+++ front/src/pages/staff/ListaBloques.jsx
@@ -2,26 +2,111 @@ import { urlApi } from "$/environment";
 import EventBlock from "$/lib/components/EventBlock";
 import axios from "axios";
 import { useLayoutEffect, useState } from "react";
-import { Stack } from "react-bootstrap";
+import {
+  Col,
+  FormControl,
+  FormGroup,
+  FormLabel,
+  Image,
+  Modal,
+  ModalBody,
+  ModalHeader,
+  ModalTitle,
+  Row,
+  Stack,
+} from "react-bootstrap";
 
 export default function ListaBloques() {
   const [eventBlocks, setEventBlocks] = useState([]);
+  const [modalEdit, setModalEdit] = useState(false);
+  const [currentEdit, setCurrentEdit] = useState({});
 
   useLayoutEffect(() => {
     axios.get(urlApi + `/eventblock/list`).then((res) => {
       if (res.data.code === 0) {
-        //console.log(res.data)
+        // console.log(res.data);
         setEventBlocks(res.data.data);
       }
     });
   }, []);
 
+  const handleChange = (campo, valor) => {
+    let temp = { ...currentEdit };
+
+    if (campo === "fecha" || campo === "hora") {
+      let tempDate = temp.datetime.split("T");
+      if (campo === "fecha") {
+        tempDate[0] = valor;
+      } else if (campo === "hora") {
+        tempDate[1] = valor;
+      }
+      temp.datetime = tempDate.join("T");
+    } else {
+      temp[campo] = valor;
+    }
+
+    setCurrentEdit(temp);
+  };
+
   return (
     <div>
+      <Modal
+        centered
+        size="xl"
+        show={modalEdit}
+        onHide={() => setModalEdit(false)}
+      >
+        <ModalHeader closeButton>
+          <ModalTitle>Editar Bloque</ModalTitle>
+        </ModalHeader>
+        <ModalBody>
+          <FormGroup>
+            <Image style={{ maxHeight: "300px" }} src={currentEdit.img} />
+            <br />
+            <FormLabel>Imagen</FormLabel>
+            <FormControl
+              type="text"
+              value={currentEdit.img}
+              onChange={(ev) => handleChange("img", ev.currentTarget.value)}
+            />
+          </FormGroup>
+          <br />
+          <Row>
+            <Col xs={12} md={6}>
+              <FormGroup>
+                <FormLabel>Fecha</FormLabel>
+                <FormControl
+                  type="date"
+                  value={currentEdit.datetime?.split("T")[0]}
+                  onChange={(ev) => handleChange("fecha", ev.currentTarget.value)}
+                />
+              </FormGroup>
+            </Col>
+            <Col xs={12} md={6}>
+              <FormGroup>
+                <FormLabel>Hora</FormLabel>
+                <FormControl
+                  type="time"
+                  value={currentEdit.datetime?.split("T")[1]}
+                  onChange={(ev) => handleChange("hora", ev.currentTarget.value)}
+                />
+              </FormGroup>
+            </Col>
+          </Row>
+          <br />
+          <ModalTitle>Eventos</ModalTitle>
+        </ModalBody>
+      </Modal>
       <h1 className="text-center">Bloques</h1>
       <Stack gap={3}>
         {eventBlocks.map((eb, i) => (
-          <EventBlock key={i} page="staff" data={eb} />
+          <EventBlock
+            key={i}
+            page="staff"
+            data={eb}
+            setModalEdit={setModalEdit}
+            setCurrentEdit={setCurrentEdit}
+          />
         ))}
       </Stack>
     </div>
