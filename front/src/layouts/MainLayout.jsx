b9b6ad611fb1a0b1ea62f76e0dcf59b7029e8ec5
jrberganza <68700999+jrberganza@users.noreply.github.com>
2024-02-05 19:52:40 -0600

    Stretch footer on long screens

--- front/src/layouts/MainLayout.jsx
+++ front/src/layouts/MainLayout.jsx
@@ -2,7 +2,7 @@ import Logo from "$/lib/components/Logo";
 import TEDxLogo from "$/lib/components/TEDxLogo";
 import { useUser } from "$/lib/hooks/user";
 import ErrorBoundary from "$/routes/ErrorBoundary";
-import { Container, Nav, Navbar } from "react-bootstrap";
+import { Container, Nav, Navbar, Stack } from "react-bootstrap";
 import { Link, Outlet } from "react-router-dom";
 
 export default function MainLayout() {
@@ -10,60 +10,62 @@ export default function MainLayout() {
 
   return (
     <ErrorBoundary>
-      <Navbar expand="lg" bg="primary" variant="dark">
-        <Container>
-          <Navbar.Brand as={Link} to="/">
-            <TEDxLogo />
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link as={Link} to="/">
-                Bloques
-              </Nav.Link>
-              {user === null ? (
-                <Nav.Link as={Link} to="/auth">
-                  Inicio de sesión
+      <Stack style={{ minHeight: "100vh" }}>
+        <Navbar expand="lg" bg="primary" variant="dark">
+          <Container>
+            <Navbar.Brand as={Link} to="/">
+              <TEDxLogo />
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="me-auto">
+                <Nav.Link as={Link} to="/">
+                  Bloques
                 </Nav.Link>
-              ) : user.userType === "staff" || user.userType === "admin" ? (
-                <Nav.Link as={Link} to="/staff">
-                  Staff
-                </Nav.Link>
-              ) : null}
-              {user !== null ? (
-                <Nav.Link
-                  onClick={() => {
-                    localStorage.clear();
-                    setTimeout(() => (window.location.href = "/"), 100);
-                  }}
-                >
-                  Cerrar sesión
-                </Nav.Link>
-              ) : null}
-            </Nav>
-            <Navbar.Text>
-              Powered by:{" "}
-              <Link to="/by">
-                <Logo /> Mycelium
-              </Link>
-            </Navbar.Text>
-          </Navbar.Collapse>
+                {user === null ? (
+                  <Nav.Link as={Link} to="/auth">
+                    Inicio de sesión
+                  </Nav.Link>
+                ) : user.userType === "staff" || user.userType === "admin" ? (
+                  <Nav.Link as={Link} to="/staff">
+                    Staff
+                  </Nav.Link>
+                ) : null}
+                {user !== null ? (
+                  <Nav.Link
+                    onClick={() => {
+                      localStorage.clear();
+                      setTimeout(() => (window.location.href = "/"), 100);
+                    }}
+                  >
+                    Cerrar sesión
+                  </Nav.Link>
+                ) : null}
+              </Nav>
+              <Navbar.Text>
+                Powered by:{" "}
+                <Link to="/by">
+                  <Logo /> Mycelium
+                </Link>
+              </Navbar.Text>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+        <Container className="py-4">
+          <ErrorBoundary>
+            <Outlet />
+          </ErrorBoundary>
         </Container>
-      </Navbar>
-      <Container className="py-4">
-        <ErrorBoundary>
-          <Outlet />
-        </ErrorBoundary>
-      </Container>
-      <div
-        className="bg-dark text-white pt-3 d-flex flex-column align-items-center gap-3"
-        style={{ paddingBottom: "10rem" }}
-      >
-        <span>Powered by Facultad de Ingenieria (FING), ISCC. 2024</span>
-        <a href="https://unis.edu.gt/">
-          <img src="https://unis.edu.gt/wp-content/uploads/2021/08/logo.svg" />
-        </a>
-      </div>
+        <div
+          className="bg-dark text-white pt-3 d-flex flex-column align-items-center gap-3 flex-grow-1"
+          style={{ paddingBottom: "10rem" }}
+        >
+          <span>Powered by Facultad de Ingenieria (FING), ISCC. 2024</span>
+          <a href="https://unis.edu.gt/">
+            <img src="https://unis.edu.gt/wp-content/uploads/2021/08/logo.svg" />
+          </a>
+        </div>
+      </Stack>
     </ErrorBoundary>
   );
 }
